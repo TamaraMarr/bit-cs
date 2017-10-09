6da28d0c4059b80f2dcc0a05bb673cc95d702eab
BIT <classroom@bgit.rs>
2017-10-09 17:00:23 +0200

    Some changes to array exercise 0

--- arrays/array0.js
+++ arrays/array0.js
@@ -1,3 +1,7 @@
 var months = ['January', 'February','March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 console.log(months[5], months[9], months[0]);
+
+var months2 = [months[5], months[9], months[0]];
+
+console.log(months2);
